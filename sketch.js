ffde7622c158ff4d036709c353646f09948ef781
jkus2017 <jkus2017@gmail.com>
2020-10-09 17:49:18 +0530

    path

--- sketch.js
+++ sketch.js
@@ -8,6 +8,9 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState = "onsling"
+
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,12 +72,14 @@ function draw(){
 }
 
 function mouseDragged(){
+    if (gameState === "onsling") {
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+  }
 }
-
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched"
 }
 
 function keyPressed(){
